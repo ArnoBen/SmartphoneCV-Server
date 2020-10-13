dc4dd0dbaee88890130fdce159e711a39fd1387e
Arno <arno.benizri@gmail.com>
2020-10-13 18:25:51 +0200

    Nice client disconnection handling

--- simple_server.py
+++ simple_server.py
@@ -1,21 +1,41 @@
 import socket
 
+
+def setup_communication(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen()
+    print("Waiting for connection...")
+    conn, addr = s.accept()
+    i = 0
+    while True:
+        try:
+            print(f"Connection address: {addr}")
+            data = conn.recv(BUFFER_SIZE)
+            print("Received data: " + data.decode("utf8"))
+            answer = f"Answer from server {i}"
+            i += 1
+            print("Sending: " + answer)
+            conn.sendall(answer.encode("utf8"))
+        except ConnectionError as error:
+            print(f"CONNECTION ERROR : {error}")
+            conn.close()
+            s.close()
+            break
+        except ConnectionAbortedError as error:
+            print(f"CONNECTION ABORTED ERROR {error}")
+        except ConnectionRefusedError as error:
+            print(f"CONNECTION REFUSED ERROR : {error}")
+        except ConnectionResetError as error:
+            print(f"CONNECTION RESET ERROR : {error}")
+
+    setup_communication(host, port)
+
+
 HOST, PORT = '', 9999
 BUFFER_SIZE = 2 ** 14
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
+
 print(f"Creating TCP server at {HOST}:{PORT}")
 
-while True:
-    s.listen()
-    conn, addr = s.accept()
-    print(f"Connection address: {addr}")
-    data = conn.recv(BUFFER_SIZE)
-    print(data.decode("utf8"))
-    answer = "Answer from server"
-    print("Sending: " + answer)
-    conn.sendall(answer.encode("utf8"))
-
-conn.close()
-socket.close()
+setup_communication(HOST, PORT)
