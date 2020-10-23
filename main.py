fe2de5ab9300ab4b567e2f1b0f81a5b976bfc2bd
ArnoBen <arno.benizri@gmail.com>
2020-10-23 21:53:27 +0200

    ce putain de main qui change

--- main.py
+++ main.py
@@ -5,46 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import simple_server
 import cv2
-
-"""
-#region MainScript
-
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    # The request handler class for our server.
-    #
-    # It is instantiated once per connection to the server, and must
-    # override the handle() method to implement communication to the
-    # client.
-
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        while True:
-            self.data = self.request.recv(2**14)  # 2**14 = 16384 bytes
-            decoded = cv2.imdecode(np.frombuffer(self.data, np.uint8), -1)
-            if decoded is not None:
-                print(f"Received image of resolution {decoded.shape}")
-                cv2.imshow("Webcam", decoded)
-                cv2.waitKey(1)
-                print("Sending answer")
-                self.request.sendall(f"Received image of shape {decoded.shape}".encode("utf-8"))
-                return
-
-
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    
-    # Create the server, binding to localhost on port 9999
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
-        server.serve_forever()
-"""
+import yolov4
 
 
 class DataProcessor:
     def __init__(self):
         self.expected_length = -1
         self.buffer = bytearray(b'')
+        self.yolo_model = yolov4.YoloDNN()
 
     def process_data(self, data):
         # print(f"Received data of length {len(data)}")
@@ -78,14 +46,12 @@ class DataProcessor:
                 else:  # Waiting for more data
                     return None
 
-
     def process_full_buffer(self):
         try:
             decoded = cv2.imdecode(np.frombuffer(self.buffer, np.uint8), -1)
             self.buffer.clear()
-            decoded = cv2.rotate(decoded, cv2.ROTATE_90_CLOCKWISE)
-            cv2.imshow("Webcam", decoded)
-            cv2.waitKey(1)
+            #decoded = cv2.rotate(decoded, cv2.ROTATE_90_CLOCKWISE)
+            self.yolo_model.get_detections(decoded)
             return f"Received image of resolution {decoded.shape}"
         except TypeError:
             error_feedback = "The data received is not an image"
@@ -96,8 +62,6 @@ class DataProcessor:
 
 
 if __name__ == "__main__":
-    cv2.namedWindow("Webcam", cv2.WINDOW_NORMAL)
-
     HOST, PORT = '', 9999
     BUFFER_SIZE = 2 ** 16
 
@@ -105,7 +69,3 @@ if __name__ == "__main__":
 
     data_processor = DataProcessor()
     server = simple_server.Server(HOST, PORT, BUFFER_SIZE, data_processor)
-
-    while True:
-        user_input = input()
-        server.send_data(user_input)
